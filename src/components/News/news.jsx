32ff38a1f11555ad8f9b0efc4fbf33d7b9e8adb4
Bhavneet Singh <bhavneet.singh@Bhavneet-Singh-L30GY09MN7.local>
2024-07-21 20:11:01 +0530

    routing

--- src/components/News/news.jsx
+++ src/components/News/news.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/header";
 
 const News = () => {
   console.log(process.env.REACT_APP_NEWS_API);
+  console.log("REACT_APP_FIREBASE_API_KEY", process.env.REACT_APP_FIREBASE_API_KEY);
   const [news, setNews] = useState([]);
   const [searchTerm, setSearchTerm] = useState("finance");
   const [loading, setLoading] = useState(false);
